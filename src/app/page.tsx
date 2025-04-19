024bf4fadfdaf0ea34647694dc9aa309556745d1
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2025-04-19 11:55:44 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -283,14 +283,13 @@ export default function Page() {
                 Contactez-moi
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Si vous souhaitez me contacter par mail, n{"'"}hésitez pas à m{"'"}envoyer un message à{" "}
+              N'hésitez pas à prendre rendez-vous pour me contacter 
                 <Link
-                  href={DATA.contact.social.email.url}
+                  href="https://calendly.com/hoyame/30min"
                   className="text-blue-500 hover:underline"
                 >
-                  l{"'"}adresse indiquée.
+                  ici !
                 </Link>
-                Je reste disponible pour répondre à toutes vos questions, échanger sur des opportunités professionnelles ou discuter de tout autre sujet pertinent. Je m{"'"}efforcerai de vous répondre dans les plus brefs délais afin d{"'"}assurer une communication efficace et fluide.
               </p>
             </div>
           </BlurFade>
