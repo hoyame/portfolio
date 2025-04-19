1db8d44086a498bc80668a0fa13d9e7bf1b4d10f
Hoyame Zouhari <56686012+hoyame@users.noreply.github.com>
2025-04-19 12:04:45 +0200

    pfpfff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -283,12 +283,12 @@ export default function Page() {
                 Contactez-moi
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              N'hésitez pas à prendre rendez-vous pour me contacter 
+              N{"'"}hésitez pas à prendre rendez-vous pour me contacter 
                 <Link
-                  href="https://calendly.com/hoyame/30min"
+                  href={"https://calendly.com/hoyame/30min"}
                   className="text-blue-500 hover:underline"
                 >
-                  ici !
+                  ici {"!"}
                 </Link>
               </p>
             </div>
